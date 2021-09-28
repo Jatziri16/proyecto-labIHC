bdc0241ba01979fa03d0f3164c31587ef0bb0978
Joaquín Carreño Rodríguez <81494112+Joaquin-CR@users.noreply.github.com>
2021-09-28 10:54:30 -0500

    Especificación del código en el área de las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegistroComponent } from './Components/registro/registro.component';
 import { MenuComponent } from './Components/menu/menu.component';
 
+//Rutas en el navegador para cada componente
 const appRoutes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'}, //Esto va a ser lo primero que se mostrará cuando no haya nada cargado
   {path: 'login', component: LoginComponent},
