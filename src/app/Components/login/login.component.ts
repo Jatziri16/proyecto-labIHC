f7915bef6308459f2a6ecbe01d6664b7baab04e0
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-10-06 23:11:47 -0500

    Se agregaron los campos para pasar la información del html al .ts

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -7,14 +8,29 @@ import { Observable } from 'rxjs';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit 
+{
+  login: FormGroup;
   items: Observable<any[]>;
-  constructor(firestore: AngularFirestore) 
+  constructor(firestore: AngularFirestore,
+              private fb:FormBuilder,) 
   { 
+    this.login = this.fb.group({
+      User: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+      Password: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.-]*$")]],
+    });
     this.items = firestore.collection('Usuarios').valueChanges();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    // 
+  }
+
+  //Funcion inicial
+  inicioSesion()
+  {
+    console.log("Está funcionando");
   }
 
 }
