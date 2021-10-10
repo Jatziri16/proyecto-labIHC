f4ac117541f4e05ee503f98e5857eaf2c7edfaed
Jatziri16 <fatimajatziri@hotmail.com>
2021-10-10 16:45:01 -0500

    Prueba de capturación de datos en el registro.ts

--- src/app/Components/registro/registro.component.ts
+++ src/app/Components/registro/registro.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent implements OnInit {
+export class RegistroComponent implements OnInit 
+{
+  datosRegistro: FormGroup;
+  nombre!: string;
+  apellido!: string;
+  correo!: string;
+  usuario!: string;
+  contra1: string = "";
+  contra2!: string;
 
-  constructor() { }
+  constructor(private fb:FormBuilder,) 
+  { 
+    this.datosRegistro = this.fb.group
+    ({
+      name: ['', [Validators.required, Validators.pattern("^[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*$")]],
+      lastName: ['', [Validators.required, Validators.pattern("^[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*$")]],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9._-]*$")]],
+      psw1: ['', [Validators.required, Validators.pattern("^[a-zA-záéíóúÁÉÍÓÚñÑ0-9&#$%()=¿?¡!._-]*$")]],
+      psw2: ['', [Validators.required, Validators.pattern("^[a-zA-záéíóúÁÉÍÓÚñÑ0-9&#$%()=¿?¡!._-]*$")]],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  registrarUser()
+  {
+    this.nombre = this.datosRegistro.value.name;
+    this.apellido = this.datosRegistro.value.lastName;
+    this.correo = this.datosRegistro.value.email;
+    this.usuario = this.datosRegistro.value.userName;
+    this.contra1 = this.datosRegistro.value.psw1;
+    this.contra2 = this.datosRegistro.value.psw2;
+    console.log("Nombre: " + this.nombre);
+    console.log("Apellido: " + this.apellido);
+    console.log("Correo: " + this.correo);
+    console.log("Usuario: " +this.usuario);
+    console.log("Contra1: " + this.contra1);
+    console.log("Contra2: " + this.contra2);
   }
 
 }
