95a739a40f0a4a30a89a915a08eb6e657b1a19c5
Jatziri16 <fatimajatziri@hotmail.com>
2021-10-13 18:13:43 -0500

    Maquetación del punto 3 (articles) del nivel 1. Teoría y Ejercicios

--- src/app/Components/n1-punto3/n1-punto3.component.ts
+++ src/app/Components/n1-punto3/n1-punto3.component.ts
@@ -9,13 +9,25 @@ export class N1Punto3Component implements OnInit
 {
   showTeoria: boolean = true;
   showEx1: boolean = false;
+  showEx2: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // Función que controla si ya puede ver el primer ejercicio después de ver la teoría
   controlEjercicio1()
   {
     this.showTeoria = false;
     this.showEx1 = true;
+    this.showEx2 = false;
+  }
+
+  // Checa las respuestas del primer ejercicio, de ser correctas se mostrará el ejercicio 2
+  checkResp1()
+  {
+    this.showTeoria = false;
+    this.showEx1 = false;
+    this.showEx2 = true;
   }
 }
