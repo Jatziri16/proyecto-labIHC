8e6d983e5f829c7a7c8ca421d468118170460968
Jatziri16 <fatimajatziri@hotmail.com>
2021-10-13 13:03:50 -0500

    Maquetación del punto 2 (verb to be) del nivel 1. Ya se agregó algo de lógica para coordinar que parte del html se mostrará (teoría, ejercicio 1 o ejercicio 2)

--- src/app/Components/n1-punto2/n1-punto2.component.ts
+++ src/app/Components/n1-punto2/n1-punto2.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './n1-punto2.component.html',
   styleUrls: ['./n1-punto2.component.css']
 })
-export class N1Punto2Component implements OnInit {
+export class N1Punto2Component implements OnInit 
+{
+  showTeoria: boolean = true;
+  showEx1: boolean = false;
+  showEx2: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  controlEjercicio1()
+  {
+    this.showTeoria = false;
+    this.showEx1 = true;
+    this.showEx2 = false;
+  }
+
+  // Checa las respuestas del primer ejercicio, de ser correctas se mostrará el ejercicio 2
+  checkResp1()
+  {
+    this.showTeoria = false;
+    this.showEx1 = false;
+    this.showEx2 = true;
+  }
+
+  // Checa las respuestas del primer ejercicio, de ser correctas se mostrará el ejercicio 2
+  checkResp2()
+  {
+    this.showTeoria = false;
+    this.showEx1 = false;
+    this.showEx2 = true;
   }
 
 }
