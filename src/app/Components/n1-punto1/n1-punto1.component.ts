61f42ce004cad1518e6f343ce737c615c9d25306
Jatziri16 <fatimajatziri@hotmail.com>
2021-10-13 21:19:33 -0500

    Maquetación del vocabulario

---
+++ src/app/Components/n1-punto1/n1-punto1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-n1-punto1',
+  templateUrl: './n1-punto1.component.html',
+  styleUrls: ['./n1-punto1.component.css']
+})
+export class N1Punto1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
