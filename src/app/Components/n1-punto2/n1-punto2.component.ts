2d29675a8bec8e908c76842fa20e5e07e39ef68d
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-10-13 20:33:50 -0500

    Corrección de código

--- src/app/Components/n1-punto2/n1-punto2.component.ts
+++ src/app/Components/n1-punto2/n1-punto2.component.ts
@@ -1,4 +1,4 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+// import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
