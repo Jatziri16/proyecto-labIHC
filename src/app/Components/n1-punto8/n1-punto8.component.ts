4ea9304d4481b4ab1d8722184792dd5201fb8b37
Joaquín Carreño Rodríguez <joaquin2010233@gmail.com>
2021-10-28 19:53:02 -0500

    Preparativos del backend del punto 8
    Se añadieron las funciones básicas para cuando se termine el HTML poder realizar posteriormente de manera más rápida el backend,

--- src/app/Components/n1-punto8/n1-punto8.component.ts
+++ src/app/Components/n1-punto8/n1-punto8.component.ts
@@ -1,15 +1,119 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CookiesServicesService } from 'src/app/Services/cookies-services.service';
+import { NivelesService } from 'src/app/Services/niveles.service';
+import { UsuarioService } from 'src/app/Services/usuario.service';
 
 @Component({
   selector: 'app-n1-punto8',
   templateUrl: './n1-punto8.component.html',
   styleUrls: ['./n1-punto8.component.css']
 })
-export class N1Punto8Component implements OnInit {
+export class N1Punto8Component implements OnInit 
+{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  usuarioFirebase: any;
+  showTeoria: boolean = true;
+  showEx1: boolean = false;
+  submitted = false;
+  bloqueo = false;
+  puntaje: number = 0;
+  constructor(private fb: FormBuilder,
+              private toastr: ToastrService,
+              private _nivelesService: NivelesService,
+              private _userService: UsuarioService,
+              private router: Router,
+              private _cookiesService: CookiesServicesService) 
+  { 
+    // 
   }
 
+  ngOnInit(): void   
+  {
+    // this.renovToken();
+  }
+
+  renovToken()
+  {
+    this._cookiesService.checkToken();
+  }
+
+  controlEjercicio1()
+  {
+    this.showTeoria = false;
+    this.showEx1 = true;
+  }
+
+  checkResp1()
+  {
+    this.submitted = true;
+    let errores = "", buenas = 0;
+    // 
+
+    if(buenas == 8)
+    {
+      this.puntaje = 60;
+      this.usuarioFirebase = this._nivelesService.accesoDatos(this._userService.USER).then(snapshot =>
+        {
+          if(snapshot.empty) 
+          {
+            this.toastr.error('No se encontro registro', 'Error');
+            return;
+          }
+          this.getID(snapshot.docs);
+        })
+
+      this.toastr.success('You are progressing very well', 'Very well!',
+      {
+        positionClass: 'toast-bottom-right',
+      });
+      this.showTeoria = false;
+      this.showEx1 = false;
+    }
+    else
+    {
+      this.toastr.warning('You have some mistakes in the question: '+errores, 'Be carfull!',
+      {
+        positionClass: 'toast-bottom-right',
+      });
+    }
+  }
+
+  getID(data: any)
+  {
+    data.forEach((doc: { data: () => any; }) =>
+    {
+      let info = doc.data();
+      if(info.Puntaje<60)
+      {
+        this._nivelesService.actualizacionPuntaje(info.ID, this.puntaje)
+        if(this.puntaje==60)
+        {
+          this.toastr.info('Now the next level is available!', 'Excelent!!',
+          {
+            positionClass: 'toast-bottom-right',
+          });
+          
+          /* Se espera 1 segundo antes de dirigirse al menu del nivel, para dar tiempo
+            de que se realice la actualizacion de los puntos correctamente. */
+          this.bloqueo = true;
+          // console.log("Espera 1 segundo");
+          setTimeout(()=>{    
+            // console.log("Listo: Ruta al menu del nivel 1")
+            this.bloqueo = false;
+            this.router.navigate(['/level1']);      
+          }, 1000); 
+        }
+      }
+      else
+      {
+        if(this.puntaje==60)
+        {
+          this.router.navigate(['/level1']);
+        }
+      }
+    });
+  }
 }
